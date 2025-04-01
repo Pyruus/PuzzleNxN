ec9533ad66bbf79fcbb6f9b6ac84dd5049f4ff9c
Pyruus <pyruuss@gmail.com>
2025-04-01 22:11:57 +0200

    Fix different size puzzle

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(int size, QWidget *parent) : QMainWindow(parent), puzzleM
 
     boardView = new BoardView(puzzleModel.getBoard(), this);
     controlPanel = new ControlPanel(this);
-    // Dodanie elementów UI
+
     QPushButton *newGameButton = new QPushButton("New Game", this);
     QLabel *boardSizeLabel = new QLabel("Board size", this);
     boardSizeComboBox = new QComboBox(this);
@@ -22,15 +22,39 @@ MainWindow::MainWindow(int size, QWidget *parent) : QMainWindow(parent), puzzleM
     layout->addWidget(boardSizeLabel, 1, 1, 1, 1);
     layout->addWidget(boardSizeComboBox, 2, 1, 1, 1);
 
-    // Połączenie sygnałów i slotów
-    connect(newGameButton, &QPushButton::clicked, [this](){
+    connect(newGameButton, &QPushButton::clicked, [this, layout](){
         int newSize = boardSizeComboBox->currentText().left(1).toInt();
-        // Usuń stary obiekt
+
         puzzleModel.~PuzzleModel();
-        // Utwórz nowy obiekt
+
         new (&puzzleModel) PuzzleModel(newSize, this);
 
-        boardView->updateBoard(puzzleModel.getBoard());
+        delete boardView;
+        boardView = new BoardView(puzzleModel.getBoard(), this);
+        layout->addWidget(boardView, 0, 0, 1, 1);
+
+        connect(boardView, &BoardView::tileClicked, [this](int x, int y) {
+            Board board = puzzleModel.getBoard();
+            int emptyX = -1, emptyY = -1;
+            for (int i = 0; i < board.getSize(); ++i) {
+                for (int j = 0; j < board.getSize(); ++j) {
+                    if (board.getTile(i, j).getValue() == 0) {
+                        emptyX = i;
+                        emptyY = j;
+                        break;
+                    }
+                }
+            }
+
+            if ((abs(x - emptyX) == 1 && y == emptyY) || (x == emptyX && abs(y - emptyY) == 1)) {
+                if (x > emptyX) { puzzleModel.moveTile(MoveDirection::UP); }
+                else if (x < emptyX) { puzzleModel.moveTile(MoveDirection::DOWN); }
+                else if (y > emptyY) { puzzleModel.moveTile(MoveDirection::LEFT); }
+                else if (y < emptyY) { puzzleModel.moveTile(MoveDirection::RIGHT); }
+            }
+        });
+
+        connect(&puzzleModel, &PuzzleModel::boardChanged, boardView, &BoardView::updateBoard);
     });
 
     connect(boardView, &BoardView::tileClicked, [this](int x, int y) {
